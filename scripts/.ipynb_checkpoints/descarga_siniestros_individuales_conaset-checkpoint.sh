f6c1ca81ec63bb6c88a2eaf58d9b827c4cdfd84c
matiasfbr <matias.bull@gmail.com>
2022-01-16 19:50:16 -0300

    Create descarga_siniestros_individuales_conaset-checkpoint.sh

---
+++ scripts/.ipynb_checkpoints/descarga_siniestros_individuales_conaset-checkpoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "../data/external"
+
+mkdir -p siniestros_individuales_conaset
+cd siniestros_individuales_conaset
+
+#Individuales RM
+
+curl -X GET "https://opendata.arcgis.com/api/v3/datasets/b9f2d0a86b3542feae6c7381fa8d80de_0/downloads/data?format=csv&spatialRefId=4326" > individuales_rm_2020.csv
+curl -X GET "https://opendata.arcgis.com/api/v3/datasets/dfea0ebe559b422cbc8700ceca5a0752_0/downloads/data?format=csv&spatialRefId=4326" > individuales_tarapaca_2020.csv
+curl -X GET "https://opendata.arcgis.com/api/v3/datasets/e03e3be3630343beb19f7ef417c50564_0/downloads/data?format=csv&spatialRefId=4326" > individuales_nuble_2020.csv
+
+
+cd ../../..
